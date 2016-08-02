4b0a2c6803fed4b67074d7081e8fab160b4bf51c
Tyler Walker <twalkerweb@gmail.com>
2016-08-02 17:47:17 -0400

    Updating to an angular application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name isunlockedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the isunlockedApp
+ */
+angular.module('isunlockedApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name isunlockedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the isunlockedApp
+ */
+angular.module('isunlockedApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
