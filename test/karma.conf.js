c1fc25d00e26b541b93e832cd1cc30a136aa55fe
Tyler Walker <twalker@eecs.wsu.edu>
2017-01-14 13:29:52 -0800

    Adding chrome plugin to karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -59,7 +59,8 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-chrome-launcher'
     ],
 
     // Continuous Integration mode
