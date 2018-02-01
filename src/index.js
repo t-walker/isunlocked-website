62d055481e1780898652007eaaebd37383466ba3
Tyler Walker <twalkerweb@gmail.com>
2018-02-01 00:43:07 -0800

    updating files for bootstrap, nav, and new layouts

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './assets/bootstrap.min.css';
+import './assets/custom.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/pages/Mission.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Mission extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Mission</h1>
+      </div>
+    );
+  }
+}
+
+export default Mission;
