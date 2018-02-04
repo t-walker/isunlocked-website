bdf39ea7fbe18f8d62c4e1f7e4d1e893b7fbfa3c
Tyler Walker <twalkerweb@gmail.com>
2018-02-03 17:41:39 -0800

    adding style/content for the main page, history, org, and navbar

--- src/pages/History.js
+++ src/pages/History.js
@@ -5,6 +5,34 @@ class History extends Component {
     return (
       <div className="container">
         <h1>History</h1>
+        <h2 className="text-info">DEF CON Unlocked</h2>
+        <p>
+          After the 22nd annual DEFCON in August of 2014, Infosec Unlocked was born from an online event called “<a href="">DEF CON Unlocked</a>” on August 20th, 2014. 
+          That day, Tarah Wheeler hosted a special event on her weekly video cast with Ryan “1o57" Clarke, Nikita Kronenberg (the DEF CON CFP chair), 
+          Jon Callas, Tamzen Cannoy, Saender “Mouse” Clark, Brad “Renderman” Haines, and Luna Lindsey. Just after DEF CON 2014, awareness began growing 
+          that few women or people of color had presented at major information security conferences that year, and Infosec Unlocked became a larger 
+          organization as volunteers continued to step up.
+        </p>
+
+        <h2 className="text-info">Year 1</h2>
+        <p>
+          The first year that ISUnlocked was at DEFCON, they held a n00b welcome party for first time attendees, 
+          hosted by AdaZebra, Tarah, and Tyler Walker. Tyler had joined ISUnlocked in the first month of the organization 
+          and helped with party arrangements and the ISUnlocked website, eventually becoming the Lead Web Developer. At 
+          this party, the first three members met Steve Christey Coley, an info sec veteran who had years of experience in 
+          the info sec conference arena. Ada, Tyler, and Steve would eventually create the core of ISUnlocked, with Tarah as 
+          an advisor and board member. During this event, the group also met Lisa Weeks, who has continued to advise the 
+          organization with PR and advocacy.
+        </p>
+
+        <h2 className="text-info">ISUniversity</h2>
+        <p>
+          Infosec Unlocked became a larger organization as volunteers continued to step up, under the leadership of initial 
+          volunteer and eventual executive director AdaZebra. In 2017, Infosec Unlocked hosted the largest n00b party at DEF 
+          CON, providing a welcoming environment to new speakers and attendees alike, and continues to host online and in-person 
+          events to help diverse speakers feel comfortable and mentored through the process of learning to speak onstage at 
+          information security events.
+        </p>
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import { Jumbotron, Button } from 'reactstrap';
-
 import logo from '../assets/logo.png';
 
 class Home extends Component {
@@ -15,41 +13,85 @@ class Home extends Component {
             </div>
             <div className="row" style={{marginTop: "20px", marginBottom: "20px"}}>
                 <div className="col-md-12">
-                    <div className="card">
-                        <div className="card-body" style={{textAlign: "center"}}>
-                            <p className="card-text">
-                                InfoSec Unlocked is the premiere resource for new speakers and diversity at InfoSec conferences.<br />
-                                If you have any questions please contact us <a href="mailto:insecunlocked@gmail.com">insecunlocked@gmail.com</a>.
+                    <p className={"text-info"} style={{textAlign: "center", fontSize: "300%"}}>
+                        Opening Doors for Diverse Voices in Information Security
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <p>
+                        InfoSec Unlocked 501(c)(3) is a non-profit organization supporting information security conferences and attendees.
+                        Our primary focus is to support new speakers get talks accepted at conferences. It's our belief that by providing mentorship
+                        and resources to new speakers we will be able to diversify the speaker pool. 
+                    </p>
+                    <p>
+                        We are always looking for passionate members of the InfoSec community to join our team and support our mission.
+                        If you are interested in joining us please consider reaching out to <a href="mailto:insecunlocked@gmail.com">insecunlocked@gmail.com</a> for more information.
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <div class="card text-white bg-secondary mb-3">
+                        <div class="card-body bg-primary">
+                            <h4 class="card-title" style={{textAlign: "center"}}>CFP Review Program</h4>
+                            <p class="card-text text-white">
+                                ISUnlocked provides a free review process for new speakers looking to get feedback before they submit. 
                             </p>
-                            
+                            <a class="btn btn-secondary btn-block" href="#">Sign-Up Now</a>
                         </div>
                     </div>
                 </div>
             </div>
+            <hr />
             <div className="row">
-                <div class="col-md-4">
-                    <h2>New Speakers</h2>
+                <div className="col-md-6">
+                    <h2>Resources for Speakers</h2>
                     <p>
-                        Speakers are the backbone of conferences, but how does one get started speaking?
-                        We have assembled resources for those interested in getting started.
+                        Looking to start speaking at conferences but don't know where to start?
+                        We provide resources and services for new speakers looking to get their start.
                     </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">Learn More &raquo;</a></p>
+                    <p><a className="btn btn-success" href="#" role="button">Learn More &raquo;</a></p>
                 </div>
-                <div class="col-md-4">
-                    <h2>Diversity</h2>
+                <div className="col-md-6">
+                    <h2>Diversity and Inclusion</h2>
                     <p>
                         Interested in fostering a more inclusive environment at your conference?
                         ISUnlocked has several resources on the best practices for conference organizers.
                     </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">Learn More &raquo;</a></p>
+                    <p><a className="btn btn-success" href="#" role="button">Learn More &raquo;</a></p>
                 </div>
-                <div class="col-md-4">
-                    <h2>Open CFPs</h2>
+            </div>
+            <hr />
+            <div className="row">
+                <div className="col-md-6">
+                    <h2>Open Call for Papers</h2>
                     <p>
-                        CFPs (Call for Papers) are where you can apply to speak at conferences.
-                        We have assembled a list of conferences speakers can currently apply to.
+                        Most conference speakers go through a CFP process where they submit directly to conferences.
+                        If you're interested in speaking at conferences, check this listing of open CFPs provided by <a href="https://twitter.com/cfp_time">@cfp_time</a>.
                     </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <p><a className="btn btn-success" href="https://twitter.com/cfp_time" target="_blank" role="button">Learn More &raquo;</a></p>
+                </div>
+                <div className="col-md-6">
+                    <h2>Conference Resources</h2>
+                    <p>
+                        Interested in fostering a more inclusive environment at your conference?
+                        ISUnlocked has several resources on the best practices for conference organizers.
+                    </p>
+                    <p><a className="btn btn-success" href="#" role="button">Learn More &raquo;</a></p>
+                </div>
+            </div>
+            <hr />
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="card border-info mb-3">
+                      <div className="card-body text-primary">
+                        <h2 className="card-title" style={{textAlign: "center"}}>How You Can Help</h2>
+                         <p className="card-text" style={{textAlign: "center"}}>
+                            If you're looking to help support the InfoSec Unlocked mission, please consider <a href="#">donating</a> or <a href="#">volunteering</a> with us.<br />
+                            All contributions are tax deductible.
+                         </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
