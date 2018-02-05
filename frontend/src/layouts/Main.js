388b31e3a40d3a27c10d162e0583f2b3cfdb0c6e
Tyler Walker <twalkerweb@gmail.com>
2018-02-05 00:51:41 -0800

    adding stuff for the node deployment

---
+++ frontend/src/layouts/Main.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Navigation from './Navbar';
+import History from '../pages/History';
+import Mission from '../pages/Mission';
+import Organization from '../pages/Organization';
+import PastEvents from '../pages/PastEvents';
+import Home from '../pages/Home';
+import Speakers from '../pages/resources/Speakers';
+import OpenCFP from '../pages/resources/OpenCFP';
+import Conferences from '../pages/resources/Conferences';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+          <Router>
+              <div>
+                <Navigation />
+                <div className="container" style={{marginTop: "10px"}}>
+                    <Switch>
+                        <Route path="/history" component={History}/>
+                        <Route path="/mission" component={Mission}/>
+                        <Route path="/organization" component={Organization}/>
+                        <Route path="/past-events" component={PastEvents}/>
+                        <Route path="/resources/speakers" component={Speakers}/>
+                        <Route path="/resources/open-cfps" component={OpenCFP}/>
+                        <Route path="/resources/conferences" component={Conferences}/>
+
+                        <Route path="/" component={Home}/>
+                    </Switch>
+                </div>
+              </div>
+          </Router>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ frontend/src/pages/PastEvents.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PastEvents extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Past Events</h1>
+      </div>
+    );
+  }
+}
+
+export default PastEvents;
