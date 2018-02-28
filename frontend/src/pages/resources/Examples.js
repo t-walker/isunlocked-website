f404b57db2abf12dc55ad729373fe1e0304f1459
Tyler Walker <twalkerweb@gmail.com>
2018-02-28 10:39:54 -0800

    updating with cfp disclaimer

--- frontend/src/pages/resources/Examples.js
+++ frontend/src/pages/resources/Examples.js
@@ -13,7 +13,17 @@ class Examples extends Component {
   renderResources() {
     let resources = [];
 
-    resources.push(<h2 className="text-primary" style={{textAlign: "center"}}>Sample CFP Submissions</h2>);
+    resources.push(
+      <h2 className="text-primary" style={{textAlign: "center"}}>
+      Sample CFP Submissions
+      </h2>
+    );
+
+    resources.push(
+      <p>
+        Disclaimer: CFP formats vary across conferences and may change year-to-year.
+      </p>
+    );
 
     for (let i = 0; i < this.resources.length; i++) {
       let link = this.resources[i];
@@ -29,6 +39,7 @@ class Examples extends Component {
         <div className="row">
           <div className="col-md-12">
             <h1>Example CFPs &amp; Talks</h1>
+  
             <br />
             {this.renderResources()}
           </div>
