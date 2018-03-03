f8fdf520be84f2082d1e3141a5e9b234e5b8e8aa
Tyler Walker <twalkerweb@gmail.com>
2018-03-02 21:26:24 -0800

    adding validations

--- server/models/CFPRequest.js
+++ server/models/CFPRequest.js
@@ -3,13 +3,15 @@ import mongoose from "../config/database";
 var Schema = mongoose.Schema;
 
 var CFPRequestSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    bio: { type: String },
-    progress: { type: String },
-    conference: { type: String },
-    additional: { type: String },
-    date: { type: Date, default: Date.now }
+    name: { type: String, required: true, maxlength: 100},
+    email: { type: String, required: true, maxlength: 320 },
+    topic: { type: String, required: true, maxlength: 50 },
+    progress: { type: String, required: true, maxlength: 50  },
+    conference: { type: String, required: true, maxlength: 50  },
+    additional: { type: String, required: true, maxlength: 2000 },
+    created: { type: Date, default: Date.now },
+    edited: { type: Date, default: Date.now },
+    secret: { type: String, required: true, maxlength: 50 }
 });
 
 module.exports = mongoose.model('CFPRequest', CFPRequestSchema);
