c70b8de0aa392cfac7be94a41f66bf1b5611d473
Tyler Walker <twalkerweb@gmail.com>
2018-09-29 15:59:19 -0700

    adding descriptions and titles

--- frontend/src/pages/Organization.js
+++ frontend/src/pages/Organization.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import TwitterHandle from '../components/TwitterHandle';
 
 class Organization extends Component {
   render() {
@@ -9,10 +8,17 @@ class Organization extends Component {
         <hr />
 
         <h2 className="text-success">Executive Committee</h2>
-        <h3>AdaZebra <TwitterHandle handle="adaZebra7"/> <small className="organization-subtitle">Executive Director</small></h3>
-        <h3>Tyler Walker <TwitterHandle handle="airercode500"/> <small className="organization-subtitle">Deputy Director</small></h3>
-        <h3>Mansi Thakar <TwitterHandle handle="mansimusa"/> <small className="organization-subtitle">Head of CFP Review Board</small></h3>
-        <h3>Steve Christey <TwitterHandle handle="SushiDude"/> <small className="organization-subtitle">Head of Community Engagement</small></h3>
+        <h3>AdaZebra <small className="organization-subtitle">Executive Director</small></h3>
+        <p>Handles all core business operations and maintains the direction of the organization</p>
+        
+        <h3>Tyler Walker <small className="organization-subtitle">Deputy Director</small></h3>
+        <p>Oversees the meeting agendas and assists with daily operations</p>
+
+        <h3>Mansi Thakar <small className="organization-subtitle">Head of CFP Review Board</small></h3>
+        <p>Manages communication between the CFP Review Board and applicants</p>
+
+        <h3>Steve Christey <small className="organization-subtitle">Head of Community Engagement</small></h3>
+        <p>Manages external relations with the InfoSec community</p>
         <hr />
         
         <h2 className="text-success">Volunteers</h2>
