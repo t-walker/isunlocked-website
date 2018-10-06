aefecf0b6c372ead7cf3eda02c412f9ad4464429
Tyler Walker <tyler.walker@sap.com>
2018-10-05 17:26:17 -0700

    updating board

--- frontend/src/pages/Organization.js
+++ frontend/src/pages/Organization.js
@@ -9,10 +9,10 @@ class Organization extends Component {
         <hr />
 
         <h2 className="text-success">Executive Committee</h2>
-        <h3>AdaZebra <TwitterHandle handle="adaZebra7"/> <small className="organization-subtitle">Executive Director</small></h3>
-        <h3>Tyler Walker <TwitterHandle handle="airercode500"/> <small className="organization-subtitle">Deputy Director</small></h3>
-        <h3>Mansi Thakar <TwitterHandle handle="mansimusa"/> <small className="organization-subtitle">Head of CFP Review Board</small></h3>
-        <h3>Steve Christey <TwitterHandle handle="SushiDude"/> <small className="organization-subtitle">Head of Community Engagement</small></h3>
+        <h3>AdaZebra <small className="organization-subtitle">Executive Director</small></h3>
+        <h3>Tyler Walker <small className="organization-subtitle">Deputy Director</small></h3>
+        <h3>Mansi Thakar <small className="organization-subtitle">Head of CFP Review Board</small></h3>
+        <h3>Steve Christey <small className="organization-subtitle">Head of Community Engagement</small></h3>
         <hr />
         
         <h2 className="text-success">Volunteers</h2>
